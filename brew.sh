4b914f760b180edeafdf971962102d0bffa6fcd1
Harsha Ramakrishna <harsha.ar@gmail.com>
2022-07-18 14:47:40 +0530

    Added homebrew download script

---
+++ brew.sh
@@ -0,0 +1,6 @@
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL [https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh](https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh))"
+else
+    brew update
